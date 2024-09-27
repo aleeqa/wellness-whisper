e4d84f32074b045f305b8abd8f61ed7dc7f001f5
afiqahaunie <167284089+afiqahaunie@users.noreply.github.com>
2024-09-27 22:31:59 +0800

    Update Service Page.il29u.js

--- src/pages/Service Page.il29u.js
+++ src/pages/Service Page.il29u.js
@@ -3,7 +3,7 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
-
+    // delete the calendar part in the section and edit the layout of the page
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
