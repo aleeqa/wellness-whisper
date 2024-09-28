c842c2a7b3a3dfbf28048c7af7d8d56b15cdecb2
FARISHAHANI <167281684+FARISHAHANI@users.noreply.github.com>
2024-09-28 15:22:05 +0800

    Update Home.dnpd4.js

--- src/pages/Home.dnpd4.js
+++ src/pages/Home.dnpd4.js
@@ -5,6 +5,14 @@ $w.onReady(function () {
     // Write your JavaScript here
     //edit title
     //edit header
+    // add logo
+    // edit profile icon
+    // edit navbar icon
+    // add image
+    // edit introduction text
+    // add button to navigate to 'service & donation' page
+    // edit footer
+    // edit background color
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
