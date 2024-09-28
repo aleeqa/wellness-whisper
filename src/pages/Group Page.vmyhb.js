1c823467c11b96a50144e737840ca85e5f5f1c5f
FARISHAHANI <167281684+FARISHAHANI@users.noreply.github.com>
2024-09-28 15:16:50 +0800

    Update Group Page.vmyhb.js

--- src/pages/Group Page.vmyhb.js
+++ src/pages/Group Page.vmyhb.js
@@ -3,7 +3,7 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
-
+    // Add new group and posts in the group
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
